2b81918f06b775697475848f0a749b478b514345
John K. Grant <john@fabric.space>
2025-11-09 11:44:49 -0800

    build: :lock: add certs to serve via https

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const devCertDir = path.resolve(__dirname, 'dev-certs')
+if (!fs.existsSync(devCertDir)) {
+  fs.mkdirSync(devCertDir, { recursive: true })
+}
 
 export default defineConfig({
+  plugins: [mkcert({
+    savePath: devCertDir,
+    keyFileName: 'dev-key.pem',
+    certFileName: 'dev-cert.pem',
+    force: false
+  })],
   base: '/device-orientation-demo/',
   root: '.',
   build: {
@@ -8,7 +25,9 @@ export default defineConfig({
     target: 'es2022'
   },
   server: {
+    host: '0.0.0.0',
     port: 3000,
+    https: true,
     open: true
   }
 })
